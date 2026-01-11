e89b4bd492a29f6003976cae40b736c4ad50b8c0
René Kramme <renekramme@gmail.com>
2026-01-11 18:39:36 +0100

    parsing arguments added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, argparse
 from dotenv import load_dotenv # type: ignore
 from google import genai # type: ignore
 
@@ -7,10 +7,19 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY not found in environment variables")
-
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.5-flash", contents = args.user_prompt)
 
+    if response.usage_metadata is not None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        raise RuntimeError("Usage metadata is missing in the response")
     print(response.text)
 
 
